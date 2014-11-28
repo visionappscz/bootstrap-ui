8a0dec01b736b2c78098fe5571ecc67e0a23ca6d
Martin Bohal <mb@mbohal.cz>
2014-11-28 22:32:29 +0100

    Added js component disable

--- dist/js/synergic-ui.js
+++ dist/js/synergic-ui.js
@@ -7,9 +7,9 @@
  * HTML & LESS © 2014 Adam Kudrna
  * JavaScript © 2014 Martin Bohal
  *
- * v0.5.0 (12 November 2014)
+ * v0.5.0 (28 November 2014)
  */
-(function($) {
+;(function($, window, document) {
   'use strict';
 
   // CONFIRMATION CLASS DEFINITION
@@ -139,9 +139,86 @@
     }
   });
 
-}(jQuery));
+}(jQuery, window, document));
 
-(function($) {
+;(function($, window, document) {
+  'use strict';
+
+  // DISABLE CLASS DEFINITION
+  // ========================
+  var Disable = function($element) {
+    this.$element = $element;
+  };
+
+  Disable.prototype.toggle = function() {
+    this.$element.prop('disabled', !this.$element.prop('disabled'));
+  };
+
+
+  // DISABLE PLUGIN DEFINITION
+  // =========================
+
+  function Plugin(options) {
+    $(document).trigger('toggle.sui.disable');
+
+    this.each(function() {
+      var $this = $(this);
+      var data = $this.data('sui.disable');
+
+      if (!data) {
+        $this.data('sui.disable', (data = new Disable($this)));
+      }
+
+      if (options === 'toggle') {
+        data.toggle();
+      }
+    });
+
+    $(document).trigger('toggled.sui.disable');
+
+    return this;
+  }
+
+
+  var old = $.fn.disable;
+
+  $.fn.disable = Plugin;
+  $.fn.disable.Constructor = Disable;
+
+
+  // DISABLE NO CONFLICT
+  // ===================
+
+  $.fn.disable.noConflict = function() {
+    $.fn.disable = old;
+    return this;
+  };
+
+
+  // DISABLE DATA-API
+  // ================
+
+  (function(Plugin, $, window, document) {
+    // We have to use $(winodow).load() as $(document).ready() can not be triggered manually
+    // and thus it would make it impossible to test this part of the code.
+    $(window).load(function() {
+      var $controls = $('[data-toggle=disable]');
+
+      $controls.each(function() {
+        var eventType = $(this).data('disable-event');
+        if (!eventType) {
+          eventType = 'change';
+        }
+        $(document).on(eventType + '.sui.disable.data-api', '[data-toggle=disable]', function() {
+          Plugin.call($($(this).data('disable-target')), 'toggle');
+        });
+      });
+    });
+  }(Plugin, $, window, document));
+
+}(jQuery, window, document));
+
+;(function($, window, document) {
     'use strict';
 
   // FILTERABLE CLASS DEFINITION
@@ -294,9 +371,9 @@
     Plugin.call($($form.data('target')), 'reset');
   });
 
-}(jQuery));
+}(jQuery, window, document));
 
-(function ($) {
+;(function ($, window, document) {
     'use strict';
 
   // SORTABLE TABLE CLASS DEFINITION
@@ -461,4 +538,4 @@
     });
   }());
 
-}(jQuery));
+}(jQuery, window, document));
