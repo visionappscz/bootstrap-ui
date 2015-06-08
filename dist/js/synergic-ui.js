47d65a878a852bc9994475cf0fce24c08de23d67
Adam Kudrna <adam@adamkudrna.cz>
2015-06-08 22:57:31 +0200

    Bump new version

--- dist/js/synergic-ui.js
+++ dist/js/synergic-ui.js
@@ -7,7 +7,7 @@
  * HTML & LESS © Adam Kudrna, 2014—2015
  * JavaScript © Martin Bohal, 2014—2015
  *
- * v0.7.0 (24 May 2015)
+ * v0.7.1 (8 June 2015)
  */
 ;(function($, window, document) {
   'use strict';
@@ -260,16 +260,16 @@
             if (typeof(dataVal) === 'string') {
               dataVal = [dataVal];
             }
+            filterValLength = filterVal.length;
             dataValLength = dataVal.length;
             for (filterValCounter = 0; filterValCounter < filterValLength; filterValCounter++) {
               for (dataValCounter = 0; dataValCounter < dataValLength; dataValCounter++) {
-                if (dataVal[dataValCounter].indexOf(filterVal[filterValCounter]) !== -1) {
+                if (dataVal[dataValCounter].toLowerCase().indexOf(filterVal[filterValCounter].toLowerCase()) !== -1) {
                   hideEl = false;
                   break;
                 }
               }
-            }
-          } else if (
+            }} else if (
             (filterOper === '=' && +dataVal !== +filterVal) ||
             (filterOper === '>=' && +dataVal < +filterVal) ||
             (filterOper === '<=' && +dataVal > +filterVal) ||
@@ -347,23 +347,32 @@
   // FILTERABLE DATA-API
   // ===================
 
-  $(document).on('change.sui.filterable.data-api', '[data-toggle=filter]', function() {
+  var lastEventTarget;
+  var lastEventValue;
+
+  $(document).on('keyup.sui.filterable.data-api change.sui.filterable.data-api', '[data-toggle=filter]', function(e) {
     var $filter = $(this).closest('form');
     var filterData = [];
 
-    $filter.find(':input').each(function() {
-      var $this = $(this);
-      if ($this.val() !== '' && $this.val() !== null) {
-        filterData.push({
-          'filter-attrib': $this.data('filter-attrib'),
-          'filter-operator': $this.data('filter-operator'),
-          'filter-value': $this.val()
-        });
-      }
-    });
+    if(lastEventTarget !== e.target || (lastEventTarget === e.target && lastEventValue !== e.target.value)) {
+      $filter.find(':input').each(function() {
+        var $this = $(this);
+        if ($this.val() !== '' && $this.val() !== null) {
+          filterData.push({
+            'filter-attrib': $this.data('filter-attrib'),
+            'filter-operator': $this.data('filter-operator'),
+            'filter-value': $this.val()
+          });
+        }
 
-    Plugin.call($($filter.data('filter-target')), filterData);
+        Plugin.call($($filter.data('filter-target')), filterData);
+      });
+    }
+
+    lastEventTarget = e.target;
+    lastEventValue = e.target.value;
   });
+
 
   $(document).on('click.sui.filterable.data-api', '[data-toggle="filter-reset"]', function() {
     var $form = $(this).closest('form');
