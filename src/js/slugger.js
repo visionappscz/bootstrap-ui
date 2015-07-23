9e14b0b4c2a9d28b52735f0c5b75ade4af8f573e
Martin Bohal <mb@mbohal.cz>
2015-07-23 16:32:59 +0200

    Add the slugger component

---
+++ src/js/slugger.js
@@ -0,0 +1,82 @@
+;(function($, window, document) {
+  'use strict';
+
+  // SLUGGER CLASS DEFINITION
+  // ========================
+  var Slugger = function($source, options) {
+    this.$source = $source;
+    this.$target = options.target;
+  };
+
+  Slugger.prototype.updateSlug = function() {
+    var generateSlug = function(str) {
+      var from = 'ãàáäâåčçďẽèéëêìíïîñõòóöôřšťùúüûýž·/_,:;';
+      var to   = 'aaaaaaccdeeeeeiiiinooooorstuuuuyz------';
+
+      str = str
+        .replace(/^\s+|\s+$/g, '') //trim
+        .toLowerCase();
+
+      for (var i = 0 ; i < from.length; i++) {
+        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+      }
+
+      str = str
+        .replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+        .replace(/\s+/g, '-') // collapse whitespace and replace by -
+        .replace(/-+/g, '-'); // collapse dashes
+
+      return str;
+    };
+
+    this.$target.val(generateSlug(this.$source.val()));
+    this.$source.trigger('updated.sui.slugger');
+  };
+
+  // SLUGGER PLUGIN DEFINITION
+  // =========================
+
+  function Plugin(options) {
+    this.each(function() {
+      var $this = $(this);
+      var data = $this.data('sui.slugger');
+
+      if (!data) {
+        data = new Slugger($this, options);
+        $this.data('sui.slugger', data);
+      }
+
+      data.updateSlug();
+    });
+
+    return this;
+  }
+
+  var old = $.fn.slugger;
+
+  $.fn.slugger = Plugin;
+  $.fn.slugger.Constructor = Slugger;
+
+  // SLUGGER NO CONFLICT
+  // ===================
+
+  $.fn.slugger.noConflict = function() {
+    $.fn.slugger = old;
+    return this;
+  };
+
+  // SLUGGER DATA-API
+  // ================
+
+  $(document)
+    .on('keyup.sui.slugger.data-api', '[data-toggle=slugger]', function() {
+      $('[data-toggle=slugger]').each(function() {
+        var $this = $(this);
+        Plugin.call($this, {target: $($this.data('slugger-target'))});
+      });
+    })
+    .on('change.sui.slugger.data-api', '[data-toggle=slugger]', function() {
+      $(this).trigger('changed.sui.slugger');
+    });
+
+}(jQuery, window, document));
