657269b3a270dda65519f1170b8d12f415bd6332
Martin Bohal <martin@visionapps.cz>
2016-06-24 14:42:29 +0200

    Improve coding style

--- src/js/tests/unit/ckeditor-loader.js
+++ src/js/tests/unit/ckeditor-loader.js
@@ -8,6 +8,7 @@ $(function () {
 
     teardown: function () {
       delete jQuery.fn.ckeditor;
+      $('html').attr('lang', null);
     },
   });
 
@@ -16,53 +17,46 @@ $(function () {
 
   // Data-api tests
   // ======================
-  QUnit.test('should call the ckeditor() method on the textarea on page load with no attribute',
-    function () {
-      var $textarea = $('<textarea data-onload-ckeditor>Some text</textarea>');
-      $('#qunit-fixture').append($textarea);
+  QUnit.test('should init ckeditor on page load with no attribute', function () {
+    var $textarea = $('<textarea data-onload-ckeditor></textarea>');
+    $('#qunit-fixture').append($textarea);
 
-      sinon.spy(jQuery.fn, 'ckeditor');
-      $(window).trigger('load');
+    sinon.spy(jQuery.fn, 'ckeditor');
+    $(window).trigger('load');
 
-      QUnit.ok(jQuery.fn.ckeditor.calledOnce, 'Should init the ckeditor');
-      QUnit.ok(
-        jQuery.fn.ckeditor.calledWithExactly({}),
-        'Should init the ckeditor with no arguments'
-      );
-    }
-  );
+    QUnit.ok(jQuery.fn.ckeditor.calledOnce, 'Should init the ckeditor');
+    QUnit.ok(
+      jQuery.fn.ckeditor.calledWithExactly({}),
+      'Should init the ckeditor with no arguments'
+    );
+  });
 
-  QUnit.test('should call the ckeditor() method on the textarea on page load with string attribute',
-    function () {
-      var $textarea = $('<textarea data-onload-ckeditor="/path/to/config.js">Some text</textarea>');
-      $('#qunit-fixture').append($textarea);
+  QUnit.test('should init ckeditor on page load with string attribute', function () {
+    var $textarea = $('<textarea data-onload-ckeditor="/path/to/config.js"></textarea>');
+    $('#qunit-fixture').append($textarea);
 
-      sinon.spy(jQuery.fn, 'ckeditor');
-      $(window).trigger('load');
+    sinon.spy(jQuery.fn, 'ckeditor');
+    $(window).trigger('load');
 
-      QUnit.ok(jQuery.fn.ckeditor.calledOnce, 'Should init the ckeditor');
-      QUnit.ok(
-        jQuery.fn.ckeditor.calledWithExactly({ customConfig: '/path/to/config.js' }),
-        'Should init the ckeditor with proper custom config file specified'
-      );
-    }
-  );
+    QUnit.ok(jQuery.fn.ckeditor.calledOnce, 'Should init the ckeditor');
+    QUnit.ok(
+      jQuery.fn.ckeditor.calledWithExactly({ customConfig: '/path/to/config.js' }),
+      'Should init the ckeditor with proper custom config file specified'
+    );
+  });
 
-  QUnit.test('should call the ckeditor() method on the textarea on page load with json attribute',
-    function () {
-      var $textarea = $(
-        '<textarea data-onload-ckeditor=\'{"option": "value"}\'>Some text</textarea>' +
-        '');
-      $('#qunit-fixture').append($textarea);
+  QUnit.test('should init ckeditor on on page load with json attribute', function () {
+    var $textarea = $('<textarea data-onload-ckeditor=\'{"option": "value"}\'></textarea>');
+    $('#qunit-fixture').append($textarea);
 
-      sinon.spy(jQuery.fn, 'ckeditor');
-      $(window).trigger('load');
+    sinon.spy(jQuery.fn, 'ckeditor');
+    $(window).trigger('load');
 
-      QUnit.ok(jQuery.fn.ckeditor.calledOnce, 'Should init the ckeditor');
-      QUnit.ok(
-        jQuery.fn.ckeditor.calledWithExactly({ option: 'value' }),
-        'Should init the ckeditor with proper config object'
-      );
-    }
-  );
+    QUnit.ok(jQuery.fn.ckeditor.calledOnce, 'Should init the ckeditor');
+    QUnit.ok(
+      jQuery.fn.ckeditor.calledWithExactly({ option: 'value' }),
+      'Should init the ckeditor with proper config object'
+    );
+  });
+
 });
